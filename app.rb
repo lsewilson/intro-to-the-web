1238f311cd1f75781d9dca22a33448c59a9e58d9
MrJons <shad_stick@hotmail.co.uk>
2016-08-30 15:52:46 +0100

    shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello world"
+end
+
+get '/secret' do
+  "I have a secret ;)"
+end
+
+get '/name' do
+  "Whats your name?"
+end
+
+get '/age' do
+  "im 22!"
 end
